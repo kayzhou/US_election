03bcd8a5dec91a7dff340f956dd010d938f103cb
Kay <zhenkun91@outlook.com>
2020-03-24 17:18:02 +0800

    u

--- analyze_hashtag.py
+++ analyze_hashtag.py
@@ -6,7 +6,7 @@
 #    By: Kay Zhou <zhenkun91@outlook.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/21 09:47:55 by Kay Zhou          #+#    #+#              #
-#    Updated: 2020/03/18 09:32:30 by Kay Zhou         ###   ########.fr        #
+#    Updated: 2020/03/24 17:17:03 by Kay Zhou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -55,11 +55,11 @@ trump_files = [
 months = set([
     "202003",
     "202002",
-    # "202001",
-    # "201912",
-    # "201911",
-    # "201910",
-    # "201909",
+    "202001",
+    "201912",
+    "201911",
+    "201910",
+    "201909",
 ])
 
 
@@ -80,6 +80,19 @@ def write_top_hashtags(in_files, out_name):
         for ht, cnt in all_hts.most_common(1000):
             print(ht, cnt, file=f)
 
+
+def write_cooccurrence_hashtags(in_files, out_name):
+    all_hts = Counter()
+    file_names = sorted(Path("raw_data").rglob("*.txt"), reverse=True)
+
+    with open(f"data/{out_name}", "w") as f:
+        for in_name in file_names:
+            if in_name.stem.split("-")[-1] in in_files and in_name.parts[1] in months:
+                print(in_name)
+                for line in tqdm(open(in_name)):
+                    hts = json.loads(line)["hashtags"]
+                    f.write(" ".join([ht["text"].lower() for ht in hts]))
+                    
 
 def get_hts(in_name):
     hts = {}
@@ -105,7 +118,8 @@ def label_based_on_before(in_name, out_name):
 
 if __name__ == "__main__":
     # write_top_hashtags(demo_files, "hashtags-democrats-20200305.txt")
-    write_top_hashtags(trump_files, "hashtags-trump-20200318.txt")
+    # write_top_hashtags(trump_files, "hashtags-trump-20200318.txt")
     # label_based_on_before("data/hashtags-democrats-20200121.txt", "data/hashtags-democrats-20200121-v2.txt")
     # label_based_on_before("data/hashtags-trump-20200121.txt", "data/hashtags-trump-20200121-v2.txt")
 
+    write_cooccurrence_hashtags(trump_files, "hashtags-co-trump-20200324.txt")
