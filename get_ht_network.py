e4197a6529389ca7ba7fa9f9dbb48d88be3465b7
Kay <zhenkun91@outlook.com>
2020-03-29 10:17:42 +0800

    u

--- get_ht_network.py
+++ get_ht_network.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    get_ht_network.py                                  :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: Kay Zhou <kayzhou.mail@gmail.com>          +#+  +:+       +#+         #
+#    By: Kay Zhou <zhenkun91@outlook.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/29 14:33:53 by Kay Zhou          #+#    #+#              #
-#    Updated: 2019/08/08 11:29:33 by Kay Zhou         ###   ########.fr        #
+#    Updated: 2020/03/29 10:17:39 by Kay Zhou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,35 +21,23 @@ import pandas as pd
 
 
 def get_hts():
-    K_ht = set()
-    M_ht = set()
-    A_ht = set()
     focus_ht = set()
-
-    for line in open("data/hashtags/2019-08-08.txt"):
+    for line in open("/twitter-analysis/model-2020-03-25/hashtags.txt"):
         w = line.strip().split()
-        ht = w[1]
-        focus_ht.add(ht)
-        if w[0] == "K":
-            K_ht.add(ht)
-        elif w[0] == "M":
-            M_ht.add(ht)
-        elif w[0] == "L":
-            A_ht.add(ht)
-    focus_ht = K_ht | M_ht | A_ht
-    print(len(K_ht), len(M_ht), len(A_ht), len(focus_ht))
-    return K_ht, M_ht, A_ht, focus_ht
+        ht, label = w[0], w[1]
+        if label != "UNK":
+            focus_ht.add(ht)
+    print(len(focus_ht))
+    return focus_ht
 
 
-K_ht, M_ht, A_ht, focus_ht = get_hts()
+focus_ht = get_hts()
 Ntweets = 0
 only_focus_count = Counter()
 hts_count = Counter()
 
-sess = get_session()
-tweets = get_all_tweets_with_hashtags(sess)
-for t in tqdm(tweets):
-    _hts = [_ht for _ht in t[1].split(",") if _ht in focus_ht]
+for line in open("data/hashtags-co-trump-20200324.txt"):
+    _hts = [_ht for _ht in line.strip().split() if _ht in focus_ht]
     if len(_hts) > 0:
         Ntweets += 1
         for i in range(len(_hts)):
@@ -60,7 +48,6 @@ for t in tqdm(tweets):
                     n1, n2 = n2, n1
                 only_focus_count[(n1, n2)] += 1
 
-sess.close()
 G = nx.Graph()
 
 for e in only_focus_count:
@@ -72,4 +59,4 @@ for n in G.node():
 G.graph["Ntweets"] = Ntweets
 
 print(G.number_of_nodes(), G.number_of_edges())
-nx.write_gpickle(G, "data/hts_20190808.gpickle")
+nx.write_gpickle(G, "data/hts_20200329.gpickle")
