297b9468dd69e9c1af78612f6578c8dbd42b15ff
Kay <zhenkun91@outlook.com>
2020-05-03 22:24:48 +0800

    u

--- prediction_reweight.py
+++ prediction_reweight.py
@@ -6,7 +6,7 @@
 #    By: Kay Zhou <zhenkun91@outlook.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/05/03 09:01:29 by Kay Zhou          #+#    #+#              #
-#    Updated: 2020/05/03 22:23:33 by Kay Zhou         ###   ########.fr        #
+#    Updated: 2020/05/03 22:24:39 by Kay Zhou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,19 +23,22 @@ USA_STATES = ['CA', 'TX', 'NY', 'FL', 'IL', 'GA', 'PA', 'OH', 'DC', 'NC', 'MI', 
 
 def load_users_location(in_name):
     users = pd.read_csv(in_name).set_index("uid")
+    print(users)
     return users
 
 
 def load_users_opinion(in_name):
     users = pd.read_csv(in_name).set_index("uid")
+    print(users)
     users.rename({0: "B", 1: "T"})
     users["Camp"] = "B" if users["B"] >= users["T"] else "T"
+    print(users)
     return users
 
 
 def load_users_union():
-    users = load_users_opinion("disk/users-location/2020-04-30.csv")
-    u2 = load_users_location("disk/users-culFrom01/2020-04-30.csv")
+    users = load_users_location("disk/users-culFrom01/2020-04-30.csv")
+    u2 = load_users_opinion("disk/users-location/2020-04-30.csv")
     users = users.join(u2, how="inner")
     return users
 
