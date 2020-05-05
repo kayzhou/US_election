d37e0ab9356b5db4db5b2655224588b265de2ee0
Kay <zhenkun91@outlook.com>
2020-05-05 16:33:36 +0800

    u

--- collect_user.py
+++ collect_user.py
@@ -6,7 +6,7 @@
 #    By: Kay Zhou <zhenkun91@outlook.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/07 20:29:42 by Kay Zhou          #+#    #+#              #
-#    Updated: 2020/03/06 08:01:40 by Kay Zhou         ###   ########.fr        #
+#    Updated: 2020/05/05 16:33:32 by Kay Zhou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,11 +21,11 @@ from collections import defaultdict
 
 import pandas as pd
 import pendulum
-import tweepy
 from dateutil.parser import parse
-from file_read_backwards import FileReadBackwards
 from tqdm import tqdm
 
+import tweepy
+from file_read_backwards import FileReadBackwards
 
 APIS_INFO = [
     {
@@ -88,6 +88,7 @@ class Twitter_Apis(object):
             api = tweepy.API(auth)
             self.Apis.append(api)
             
+             
     def need_one(self):
         while True:
             for i in range(len(self.Apis)):
@@ -97,140 +98,34 @@ class Twitter_Apis(object):
 def WriteThem(rsts, opened_file):
     for r in rsts:
         opened_file.write(json.dumps(r._json) + "\n")
+
+
+def GetThem(user_list):
+    # 每次获取100个
+    Apis = Twitter_Apis().need_one()
+
+    for i in range(int(len(user_list) / 100)):
+        try:
+            print(i * 100)
+            time.sleep(0.1)
+            api = next(Apis)
+            r = api.lookup_users(user_ids=user_list[i * 100: (i + 1) * 100], include_entities=False, tweet_mode="extended")
+            print(r)
+        except Exception as e:
+            # print(type(e))
+            print("Exceptions:", e)
     
-    
-def GetThem(user_list, out_file=""):
-    
-    with open(f'disk/user_profile/{out_file}', 'w') as f:
-        Apis = Twitter_Apis().need_one()
-
-        for i in range(int(len(user_list) / 100)):
-            try:
-                print(i * 100)
-                time.sleep(0.1)
-                api = next(Apis)
-                rsts = api.lookup_users(user_ids=user_list[i * 100: (i + 1) * 100], include_entities=False, tweet_mode="extended")
-                WriteThem(rsts, f)
-            except Exception as e:
-                # print(type(e))
-                print("Exceptions:", e)
-        
-        api = next(Apis)
-        rsts = api.lookup_users(user_ids=user_list[i * 100:], include_entities=False, tweet_mode="extended")
-        WriteThem(rsts, f)
+    time.sleep(0.1)
+    api = next(Apis)
+    r = api.lookup_users(user_ids=user_list[i * 100:], include_entities=False, tweet_mode="extended")
+    print(r)
 
 
-def read_end_file(start, end):
+def get_user_list():
 
-    set_users = set()
-    target_dir = ["201907"]
-    cnt = 0
+    have_face = set(str(json.loads(line.strip())["id"]) for line in open("disk/users-face/2020-04-24.lj"))
+    user_list = [line.strip().split(",")[0] for line in open("disk/users-location/2020-04-30.csv")]
+    user_list = [uid for uid in user_list if uid not in have_face]
+    print("Run:", len(user_list))
 
-    for _dir in target_dir:
-        for in_name in tqdm(os.listdir("data/" + _dir)):
-            if in_name.endswith("PRO.txt") or in_name.endswith("Moreno.txt") \
-                or in_name.endswith("Sola.txt") or in_name.endswith("PASO.txt"):
-                continue
-            
-            print(in_name, "start ...")
-            in_name = "data/" + _dir + "/" + in_name
-
-            with FileReadBackwards(in_name) as f:
-                while True:
-                    line = f.readline()
-                    if not line:
-                        print("end of", in_name, cnt)
-                        break
-
-                    d = json.loads(line.strip())
-
-                    dt = pendulum.from_format(
-                        d["created_at"], 'ddd MMM DD HH:mm:ss ZZ YYYY')
-                    if dt < start:
-                        print("sum:", cnt, d["created_at"], "end!")
-                        break
-                    if dt >= end:
-                        continue
-
-                    if cnt % 10000 == 0:
-                        print("New data:", cnt)
-                    cnt += 1
-
-                    if d["user"]["id"] in set_users:
-                        continue
-                    set_users.add(d["user"]["id"])
-
-                    yield d["user"]
-
-
-def read_all_files():
-    set_users = set()
-    target_dir = ["201902", "201903", "201904", "201905", "201906", "201907"]
-    cnt = 0
-
-    for _dir in target_dir:
-        for in_name in tqdm(os.listdir("data/" + _dir)):
-            if in_name.endswith("PRO.txt") or in_name.endswith("Moreno.txt") \
-                or in_name.endswith("Sola.txt") or in_name.endswith("PASO.txt"):
-                continue
-            
-            print(in_name, "start ...")
-            in_name = "data/" + _dir + "/" + in_name
-
-            for line in open(in_name):
-
-                d = json.loads(line.strip())
-
-                if d["user"]["id"] in set_users:
-                    continue
-                set_users.add(d["user"]["id"])
-
-                yield d["user"]
-
-
-def get_user_profile_from_raw_file(out_file="raw"):
-    # file_name = f'data/user_profile/0731-week-{out_file}.txt'
-    # f = open(file_name, 'w')
-    # start = pendulum.datetime(2019, 7, 24, tz="UTC") # include this date
-    # end = pendulum.datetime(2019, 7, 31, tz="UTC") # not include this date
-    # for u in read_end_file(start, end):
-    #     f.write(json.dumps(u, ensure_ascii=False) + "\n")
-
-    file_name = 'data/user_profile/02-07-all.txt'
-    with open(file_name, "w") as f:
-        for u in read_all_files():
-            f.write(json.dumps(u, ensure_ascii=False) + "\n")
-
-
-def main():
-    # get_user_profile_from_raw_file()
-    # user_list = []
-    # for line in open("../russian_trolls/data/influencers/C1-3.txt"):
-    #     line = line.strip()
-    #     if line.endswith("Name Not Found"):
-    #         uid = line.split()[1]
-    #         user_list.append(uid)
-
-    # data = json.load(open("data/users/2019-07-26.txt"))
-    # for k, v in data.items():
-    #     # if (v["M"] > 30 and v["K"] < 10) or (v["K"] > 30 and v["M"] < 10):
-    #     user_list.append(k)
-    # GetThem(user_list)
-
-    # have_them = set()
-    # with open("data/users_0731.json", "w") as f:
-    #     for line in open("data/user_profile/2019-07-25-M&K.txt"):
-    #         d = json.loads(line.strip())
-
-    #         if d["screen_name"] in user_list and d["id"] not in have_them:
-    #             d["user_URL"] = "https://twitter.com/" + d["screen_name"]
-    #             f.write(json.dumps(d, indent=2) + "\n\n")
-    #             have_them.add(d["id"])
-    
-    users = pd.read_pickle("disk/data/df_users_remove_hillary_p=.7.pl").reset_index()
-    users_list = users["uid"].to_list()
-    GetThem(users_list, "2016election.json")
-
-
-if __name__ == '__main__':
-    main()
+    GetThem(user_list)
