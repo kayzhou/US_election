a517ef6855af1eb7a81ec45751d5f304d94c5e8a
Zhenkun <zhenkun91@outlook.com>
2020-07-10 16:27:08 +0800

    u

--- daily_predict.py
+++ daily_predict.py
@@ -34,7 +34,7 @@ def remove_yesterday_temp_files(dt):
 
 
 def daily_election():
-    # sess = get_session()
+    sess = get_session()
     end = pendulum.today(tz="UTC")
     start = pendulum.yesterday(tz="UTC")
     print(f"{start} <= run < {end} ")
@@ -44,17 +44,17 @@ def daily_election():
 
     # Save user snapshot
     save_user_snapshot_perday(sess, start)
-    # sess.close()
+    sess.close()
 
     # New users with location
-    write_users_today(end)
-    write_users_today_csv(end)
-    write_users_today_face_csv(end)
-    remove_yesterday_temp_files(start)
+    # write_users_today(end)
+    # write_users_today_csv(end)
+    # write_users_today_face_csv(end)
+    # remove_yesterday_temp_files(start)
 
     # Predict
     # calculate_window_share(start, end) # make *.csv
-    calculate_cumulative_share(start, end) # make *.csv
+    calculate_cumulative_share(start, end, super_start_month="01", save_csv=False, save_users=True, save_db=True) # make *.csv
 
     # demo_predict_to_db(end, clear=False)
 
