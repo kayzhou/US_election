98869d098e2ca86a456c499f0af0ac66dc18e6a7
Zhenkun <zhenkun91@outlook.com>
2020-08-05 20:22:17 +0800

    u

--- analyze_hashtag.py
+++ analyze_hashtag.py
@@ -79,6 +79,10 @@ def write_top_hashtags_mex(out_name):
 
     for in_name in file_names:
         print(in_name)
+        if in_name.endswith("202007-(Sinhue AND presidente) OR.txt"):
+            continue
+        if "202007_hold" in in_name:
+            continue
         for line in tqdm(open(in_name)):
             try:
                 hts = json.loads(line)["hashtags"]
