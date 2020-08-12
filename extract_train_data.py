ddcd885e4812fe465f63bdea417dd0286d59bddd
Kay <zhenkun91@outlook.com>
2020-08-12 19:15:18 +0800

    u

--- extract_train_data.py
+++ extract_train_data.py
@@ -6,7 +6,7 @@
 #    By: Zhenkun <zhenkun91@outlook.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/21 09:47:55 by Kay Zhou          #+#    #+#              #
-#    Updated: 2020/08/12 19:09:59 by Zhenkun          ###   ########.fr        #
+#    Updated: 2020/08/12 19:15:07 by Zhenkun          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -180,61 +180,71 @@ if __name__ == "__main__":
     DT_hts = classified_hts["DT"]
     JB_hts_c = category_hts["JB"]
     DT_hts_c = category_hts["DT"]
+    print(len(JB_hts), len(JB_hts_c), len(DT_hts), len(DT_hts_c))
 
     with open(train_dir + "train.txt", "w") as f: # 2020-03-06
-        for dt_dir in Path("raw_data").iterdir():
-            set_id = set() # remove dups
-            for in_name in dt_dir.iterdir():
-                if in_name.stem.split("-")[-1] in election_files and in_name.parts[1] in months:
-                    print(in_name)
-                    for line in tqdm(open(in_name)):
-                        label_bingo_times = 0
-                        label = None
-                        data = json.loads(line.strip())
-                        
-                        # ignoring retweets
-                        if 'retweeted_status' in data and data["text"].startswith("RT @"): 
-                            continue
-                        set_hts = set([ht["text"].lower() for ht in data["hashtags"]])
-                        if not set_hts:
-                            continue
-                            
-                        if data["id"] in set_id:
-                            continue
-                        set_id.add(data["id"])
+        # for dt_dir in Path("raw_data").iterdir():
+        set_id = set()  # remove dups
+        #     for in_name in dt_dir.iterdir():
+        #         if in_name.stem.split("-")[-1] in election_files and in_name.parts[1] in months:
+        #             print(in_name)
 
-                        for _ht in set_hts:
-                            if _ht in JB_hts:
-                                label_bingo_times += 1
-                                label = "JB"
-                                break
-                        for _ht in set_hts:
-                            if _ht in DT_hts:
-                                label_bingo_times += 1
-                                label = "DT"
-                                break
+        months = ["202001", "202002", "202003", "202004", "202005", "202006"]
+        for month in months:
+            print(month)
+            for line in tqdm(open(f"/media/alex/data/US2020_raw/{month}.lj")):
+                try:
+                    data = json.loads(line.strip())
+                except Exception:
+                    print('json.loads Error:', line)
+                    continue
 
-                        if not (label and label_bingo_times == 1):
-                            continue
+                label_bingo_times = 0
+                label = None
+                
+                # ignoring retweets
+                if 'retweeted_status' in data and data["text"].startswith("RT @"): 
+                    continue
+                set_hts = set([ht["text"].lower() for ht in data["hashtags"]])
+                if not set_hts:
+                    continue
+                    
+                if data["id"] in set_id:
+                    continue
+                set_id.add(data["id"])
 
-                        # 判断类别
-                        cate = "UNK"
-                        cate_bingo_times = 0
-                        for _ht in set_hts:
-                            if _ht in JB_hts_c:
-                                cate_bingo_times += 1
-                                cate = "JB"
-                                break
-                        for _ht in set_hts:
-                            if _ht in DT_hts_c:
-                                cate_bingo_times += 1
-                                cate = "DT"
-                                break
-                        
-                        if cate_bingo_times == 2:
-                            cate = "JB+DT"
-                            
-                        # one tweet (in traindata) should have 0 or 1 class hashtag
-                        if label and label_bingo_times == 1:
-                            text = data["text"].replace("\n", " ").replace("\t", " ")
-                            f.write(label + "\t" + cate + "\t" + text + "\n")
+                for _ht in set_hts:
+                    if _ht in JB_hts:
+                        label_bingo_times += 1
+                        label = "JB"
+                        break
+                for _ht in set_hts:
+                    if _ht in DT_hts:
+                        label_bingo_times += 1
+                        label = "DT"
+                        break
+
+                if not (label and label_bingo_times == 1):
+                    continue
+
+                # 判断类别
+                cate = "UNK"
+                cate_bingo_times = 0
+                for _ht in set_hts:
+                    if _ht in JB_hts_c:
+                        cate_bingo_times += 1
+                        cate = "JB"
+                        break
+                for _ht in set_hts:
+                    if _ht in DT_hts_c:
+                        cate_bingo_times += 1
+                        cate = "DT"
+                        break
+                
+                if cate_bingo_times == 2:
+                    cate = "JB+DT"
+                    
+                # one tweet (in traindata) should have 0 or 1 class hashtag
+                if label and label_bingo_times == 1:
+                    text = data["text"].replace("\n", " ").replace("\t", " ")
+                    f.write(label + "\t" + cate + "\t" + text + "\n")
