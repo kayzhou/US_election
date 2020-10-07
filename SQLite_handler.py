3307cb80a9a67f644a8ad472fc5675e19ce04eaf
Kay <zhenkun91@outlook.com>
2020-10-07 19:03:10 +0800

    u

--- SQLite_handler.py
+++ SQLite_handler.py
@@ -6,7 +6,7 @@
 #    By: Zhenkun <zhenkun91@outlook.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/07 20:40:05 by Kay Zhou          #+#    #+#              #
-#    Updated: 2020/10/07 19:01:32 by Zhenkun          ###   ########.fr        #
+#    Updated: 2020/10/07 19:03:05 by Zhenkun          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -2347,11 +2347,13 @@ def get_tweets_August_July():
     with open(f"data/202008-tweets-prediction.txt", "w") as f:
         start = pendulum.datetime(2020, 8, 1, 4)
         end = pendulum.datetime(2020, 9, 1, 4)
-        for _d in tqdm(get_tweets_all(sess, start, start.add(days=1))):
-            if _d.camp == 0:
-                _d.max_proba = 1 - float(_d.max_proba)
-            _d.dt = pendulum.instance(_d.dt).add(hours=-4)
-            f.write(f"{_d.tweet_id},{_d.user_id},{_d.dt.to_datetime_string()},{_d.source},{_d.max_proba}\n")
+        while True:
+            for _d in tqdm(get_tweets_all(sess, start, start.add(days=1))):
+                if _d.camp == 0:
+                    _d.max_proba = 1 - float(_d.max_proba)
+                _d.max_proba = round(_d.max_proba, 3)
+                _d.dt = pendulum.instance(_d.dt).add(hours=-4)
+                f.write(f"{_d.tweet_id},{_d.user_id},{_d.dt.to_datetime_string()},{_d.source},{_d.max_proba}\n")
             start = start.add(days=1)
             if start >= end:
                 break
@@ -2359,11 +2361,13 @@ def get_tweets_August_July():
     with open(f"data/202007-tweets-prediction.txt", "w") as f:
         start = pendulum.datetime(2020, 7, 1, 4)
         end = pendulum.datetime(2020, 8, 1, 4)
-        for _d in tqdm(get_tweets_all(sess, start, start.add(days=1))):
-            if _d.camp == 0:
-                _d.max_proba = 1 - float(_d.max_proba)
-            _d.dt = pendulum.instance(_d.dt).add(hours=-4)
-            f.write(f"{_d.tweet_id},{_d.user_id},{_d.dt.to_datetime_string()},{_d.source},{_d.max_proba}\n")
+        while True:
+            for _d in tqdm(get_tweets_all(sess, start, start.add(days=1))):
+                if _d.camp == 0:
+                    _d.max_proba = 1 - float(_d.max_proba)
+                _d.max_proba = round(_d.max_proba, 3)
+                _d.dt = pendulum.instance(_d.dt).add(hours=-4)
+                f.write(f"{_d.tweet_id},{_d.user_id},{_d.dt.to_datetime_string()},{_d.source},{_d.max_proba}\n")
             start = start.add(days=1)
             if start >= end:
                 break
