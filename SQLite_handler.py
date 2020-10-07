a4d557ea52f1b968be7731a7e2c164657e629895
Kay <zhenkun91@outlook.com>
2020-10-07 10:23:04 +0800

    u

--- SQLite_handler.py
+++ SQLite_handler.py
@@ -6,7 +6,7 @@
 #    By: Zhenkun <zhenkun91@outlook.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/07 20:40:05 by Kay Zhou          #+#    #+#              #
-#    Updated: 2020/10/07 10:21:38 by Zhenkun          ###   ########.fr        #
+#    Updated: 2020/10/07 10:22:55 by Zhenkun          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -2347,9 +2347,9 @@ def get_tweets_August_July():
     end = pendulum.datetime(2020, 9, 1, 4)
     
     with open(f"data/202008-tweets-prediction.txt", "w") as f:
-        for _d in get_tweets_all(sess, start, end):
+        for _d in tqdm(get_tweets_all(sess, start, end)):
             if _d.camp == 0:
-                _d.max_proba = 1 - _d.max_proba
+                _d.max_proba = 1 - float(_d.max_proba)
             _d.dt = pendulum.instance(_d.dt).add(hours=-4)
             f.write(f"{_d.tweet_id},{_d.user_id},{_d.dt.to_datetime_string()},{_d.source},{_d.max_proba}\n")
 
@@ -2357,9 +2357,9 @@ def get_tweets_August_July():
     end = pendulum.datetime(2020, 8, 1, 4)
     
     with open(f"data/202007-tweets-prediction.txt", "w") as f:
-        for _d in get_tweets_all(sess, start, end):
+        for _d in tqdm(get_tweets_all(sess, start, end)):
             if _d.camp == 0:
-                _d.max_proba = 1 - _d.max_proba
+                _d.max_proba = 1 - float(_d.max_proba)
             _d.dt = pendulum.instance(_d.dt).add(hours=-4)
             f.write(f"{_d.tweet_id},{_d.user_id},{_d.dt.to_datetime_string()},{_d.source},{_d.max_proba}\n")
 
