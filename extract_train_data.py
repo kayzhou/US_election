b5f46b7fd4117dfae72141288b1238fa88fb0b0d
Kay <zhenkun91@outlook.com>
2020-10-19 12:03:30 +0800

    u

--- extract_train_data.py
+++ extract_train_data.py
@@ -6,7 +6,7 @@
 #    By: Zhenkun <zhenkun91@outlook.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/21 09:47:55 by Kay Zhou          #+#    #+#              #
-#    Updated: 2020/10/19 11:00:20 by Zhenkun          ###   ########.fr        #
+#    Updated: 2020/10/19 12:01:28 by Zhenkun          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,40 +17,32 @@ from tqdm import tqdm
 
 train_dir = "data/train-10/"
 
-# demo_files = set([
-#     "Bernie Sanders",
-#     "SenSanders",
-#     "Joe Biden",
-#     "JoeBiden",
-#     # "Mike Bloomberg",
-#     # "MikeBloomberg",
-#     # "Tulsi Gabbard",
-#     # "TulsiGabbard",
-#     # "Elizabeth Warren",
-#     # "ewarren",
-#     # "Amy Klobuchar",
-#     # "amyklobuchar",  
-#     # "Pete Buttigieg",
-#     # "PeteButtigieg",
-#     # "John Delaney",
-#     # "JohnDelaney",
-#     # "Tulsi Gabbard",
-#     # "TulsiGabbard",
-#     # "Amy Klobuchar",
-#     # "amyklobuchar",
-#     # "Deval Patrick",
-#     # "DevalPatrick",
-#     # "Bernie Sanders",
-#     # "SenSanders",
-#     # "Tom Steyer",
-#     # "TomSteyer",
-#     # "Elizabeth Warren",
-#     # "ewarren",
-#     # "Andrew Yang",
-#     # "AndrewYang",
-#     "Donald Trump",
-#     "realDonaldTrump",
-# ])
+demo_files = set([
+    "Bernie Sanders",
+    "SenSanders",
+    "Joe Biden",
+    "JoeBiden",
+    "Mike Bloomberg",
+    "MikeBloomberg",
+    "Tulsi Gabbard",
+    "TulsiGabbard",
+    "Elizabeth Warren",
+    "ewarren",
+    "Amy Klobuchar",
+    "amyklobuchar",  
+    "Pete Buttigieg",
+    "PeteButtigieg",
+    "John Delaney",
+    "JohnDelaney",
+    "Deval Patrick",
+    "DevalPatrick",
+    "Tom Steyer",
+    "TomSteyer",
+    "Andrew Yang",
+    "AndrewYang",
+    "Donald Trump",
+    "realDonaldTrump",
+])
 
 election_files = set([
     "Biden"
@@ -136,7 +128,7 @@ def ext_1():
     """
     classified_hts, category_hts = read_classified_hashtags()
     with open(train_dir + "train.txt", "w") as f:
-        months = ["202001", "202002", "202003", "202004", "202005", "202006", "202007", "202008"]
+        months = ["202004", "202005", "202006"]
         for month in months:
             print(month)
             if month in ["202001", "202002"]:
@@ -171,6 +163,50 @@ def ext_1():
                 if label and label_bingo_times == 1:
                     text = data["text"].replace("\n", " ").replace("\t", " ")
                     f.write(label + "\t" + text + "\n")
+
+
+def ext_1_Jan():
+    """
+    只考虑第一列，支持或反对
+    """
+    classified_hts, category_hts = read_classified_hashtags()
+    with open(train_dir + "train-from-Jan-to-March.txt", "w") as f:
+        months = ["202001", "202002", "202003"]
+
+        file_names = sorted(Path("raw_data").rglob("*.txt"), reverse=True)
+        set_tweets = set()
+        for in_name in file_names:
+            if in_name.stem.split("-")[-1] in election_files and in_name.parts[1] in months:
+                for line in tqdm(open(in_name)):       
+                    label_bingo_times = 0
+                    label = None
+                    try:
+                        data = json.loads(line)
+                    except Exception:
+                        print("json.loads ERROR:", line)
+                        
+                    # ignoring retweets
+                    if 'retweeted_status' in data and data["text"].startswith("RT @"): 
+                        continue
+                    set_hts = set([ht["text"].lower() for ht in data["hashtags"]])
+                    # 如果没有hashtags
+                    if not set_hts:
+                        continue
+                    if data["id"] in set_tweets:
+                        continue
+                    set_tweets.add(data["id"])
+
+                    for _label, _set_hts in classified_hts.items():
+                        for _ht in set_hts:
+                            if _ht in _set_hts:
+                                label = _label
+                                label_bingo_times += 1
+                                break
+                                
+                    # one tweet (in traindata) should have 0 or 1 class hashtag
+                    if label and label_bingo_times == 1:
+                        text = data["text"].replace("\n", " ").replace("\t", " ")
+                        f.write(label + "\t" + text + "\n")
 
 
 def ext_2():
@@ -236,5 +272,6 @@ def ext_2():
 
 
 if __name__ == "__main__":
+    ext_1_Jan()
     ext_1()
     # ext_2()
